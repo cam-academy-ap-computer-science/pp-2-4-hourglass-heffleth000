6ada1b1356bdcf71c4e0b105ca0a7821b7f5ff8a
heffleth000 <heffleth000@CAM-LAB-28.bgsd.k12.wa.us>
2019-10-10 07:46:01 -0700

    Done including scaling

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -10,11 +10,16 @@
 		use nested for loop for the : in the middle
 		surround by / and \ with concatenation
 	Use the top/bottom line to finish
+	
+	FOR THE SCALE:
+	To change scale, change what SIZE equals at the beginning of code from
+	4 to however tall you want the hourglass to be. As of now, the sloping
+	sides of the hourglass are 4 lines tall going up and going down. SIZE
+	represents this number.
 */
 
 public class Hourglass {
-
-	
+	public static final int SIZE = 4;
 	public static void main(String[] args) {
 		fullLine();
 		top();
@@ -26,7 +31,7 @@ public class Hourglass {
 	//will make both the top and bottom lines
 	public static void fullLine() {
 		System.out.print("|");
-		for(int x = 1; x <= 10; x++) {
+		for(int x = 1; x <= 2 * SIZE + 2; x++) {
 			System.out.print("\"");
 		}
 		System.out.println("|");
@@ -34,13 +39,13 @@ public class Hourglass {
 	
 	//will make the top half of the hourglass
 	public static void top() {
-		for (int y = 1; y <= 4; y++) {
+		for (int y = 1; y <= SIZE; y++) {
 			for (int space = 1; space <= y; space++) {		//creating spaces inside
 				System.out.print(" ");
 			}
 			System.out.print("\\");
-			for (int dots = 10 - (2 * y); dots >=1; dots--) {	//creating dots inside
-				System.out.print(":");
+			for (int dots = (2 * SIZE + 2) - (2 * y); dots >=1; dots--) {	//creating dots inside  
+				System.out.print(":");						//if scaling doesn't work, original equation: 10 - (2 * y)
 			}
 			System.out.println("/");
 		}
@@ -48,17 +53,20 @@ public class Hourglass {
 	
 	//will make lines in middle
 	public static void middle() {
-		System.out.println("     ||");
+		for(int space = 1; space <= SIZE + 1; space++) {
+			System.out.print(" ");
+		}
+		System.out.println("||");
 	}
 	
 	//will make bottom half of hourglass
 	public static void bottom() {									
-		for (int b = 4; b >= 1; b--) {
+		for (int b = SIZE; b >= 1; b--) {
 			for (int space = 1; space <= b; space++) {
 				System.out.print(" ");
 			}
 			System.out.print("/");									
-			for (int dots = 10 - (2 * b); dots >= 1; dots--) {
+			for (int dots = (2 * SIZE + 2) - (2 * b); dots >= 1; dots--) {
 				System.out.print(":");
 			}
 			System.out.println("\\");
